447496916f63055707ec758409efa5bbc05069c8
Eric Li <ericlibeibei@gmail.com>
2025-09-15 17:54:31 -0400

    d

--- express-server.js
+++ express-server.js
@@ -1,17 +1,20 @@
 const express = require('express');
-require('dotenv').config();
 const { connectDB, getCollection } = require('./server/db.cjs');
 const { ObjectId } = require('mongodb');
 
 const app = express();
-const port = 3000;
-
-// Serve static files
+app.use(express.json());
 app.use(express.static('public'));
 app.use(express.static('views'));
-app.use(express.json());
 
-let tasksCollection; // Make it global so routes can access it
+let tasksCollection;
+
+async function initDB() {
+  if (!tasksCollection) {
+    await connectDB();
+    tasksCollection = getCollection('tasks-collection');
+  }
+}
 
 function calculateDaysLeft(taskDueDate) {
   const dueDate = new Date(taskDueDate);
@@ -19,74 +22,51 @@ function calculateDaysLeft(taskDueDate) {
   return Math.ceil((dueDate - today) / (1000 * 60 * 60 * 24));
 }
 
-async function startServer() {
-  try {
-    const db = await connectDB();
-    tasksCollection = getCollection("tasks-collection");
-
-    app.listen(port, () => {
-      console.log(`Server running at http://localhost:${port}/`);
-    });
-  } catch (err) {
-    console.error("Failed to start server:", err);
-  }
-}
-
+// Routes
 app.get('/todos', async (req, res) => {
-  try {
-    const todos = await tasksCollection.find({}).toArray();
-    res.json(todos);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  await initDB();
+  const todos = await tasksCollection.find({}).toArray();
+  res.json(todos);
 });
 
-// Routes
-app.post("/submit", async (req, res) => {
-  try {
-    const todo = req.body;
-    todo.daysLeft = calculateDaysLeft(todo.taskDueDate);
-    await tasksCollection.insertOne(todo);
-
-    const todos = await tasksCollection.find({}).toArray();
-    res.json(todos);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+app.post('/submit', async (req, res) => {
+  await initDB();
+  const todo = req.body;
+  todo.daysLeft = calculateDaysLeft(todo.taskDueDate);
+  await tasksCollection.insertOne(todo);
+  const todos = await tasksCollection.find({}).toArray();
+  res.json(todos);
 });
 
 app.delete('/delete', async (req, res) => {
-  try {
-    const { id } = req.query;
-    await tasksCollection.deleteOne({ _id: new ObjectId(id) });
-
-    const todos = await tasksCollection.find({}).toArray();
-    res.json(todos); // send updated list
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  await initDB();
+  const { id } = req.query;
+  await tasksCollection.deleteOne({ _id: new ObjectId(id) });
+  const todos = await tasksCollection.find({}).toArray();
+  res.json(todos);
 });
 
 app.post('/toggle', async (req, res) => {
+  await initDB();
   const { id, completed } = req.body;
   await tasksCollection.findOneAndUpdate(
     { _id: new ObjectId(id) },
     { $set: { completed } }
   );
-
   const todos = await tasksCollection.find({}).toArray();
   res.json(todos);
 });
 
 app.post('/edit', async (req, res) => {
+  await initDB();
   const { id, taskTitle, taskDescription, taskDueDate } = req.body;
   await tasksCollection.findOneAndUpdate(
     { _id: new ObjectId(id) },
     { $set: { taskTitle, taskDescription, taskDueDate } }
   );
-
   const todos = await tasksCollection.find({}).toArray();
   res.json(todos);
 });
 
-startServer();
+// Export the app for Vercel serverless
+module.exports = app;
